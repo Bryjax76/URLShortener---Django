a0cf7bfabde541a0d2b78618afa7ba29969a0592
Bryjax <michal.bryja@outlook.com>
2022-07-28 11:57:19 +0200

    CSS

--- Project/project_urlshortener/project_urlshortener/settings.py
+++ Project/project_urlshortener/project_urlshortener/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_PACKAGES = [
+    'app_urlshortener'
+]
 
+THIRD_PARTY_PACKAGES = [
+
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_urlshortener'
-]
+] + INTERNAL_PACKAGES + THIRD_PARTY_PACKAGES
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
